60b936af5e84f8a1b1cbbc8443bec137df70208a
Zakaria Asad <zasad2587@gmail.com>
2024-10-31 23:24:09 +0200

    Improve the user experience for show User not found if some one enter invaild user

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,25 +21,33 @@ const HomePage = () => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-4 my-4">
-      {filteredUsers?.map((user) => (
-        <Link
-          to={`/user/${user.id}`}
-          key={user.id}
-          className="border border-gray-300 rounded-lg p-4 bg-white shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl cursor-pointer"
-        >
-          <img
-            src={user.avatar}
-            alt={`${user.first_name} ${user.last_name}`}
-            className="w-24 h-24 rounded-full object-cover mb-4 shadow-md"
-          />
-          <div className="text-center">
-            <p className="text-lg font-semibold text-gray-900">
-              {user.first_name} {user.last_name}
-            </p>
-            <p className="text-sm text-gray-600">{user.email}</p>
-          </div>
-        </Link>
-      ))}
+      {filteredUsers && filteredUsers.length > 0 ? (
+        filteredUsers?.map((user) => (
+          <Link
+            to={`/user/${user.id}`}
+            key={user.id}
+            className="border border-gray-300 rounded-lg p-4 bg-white shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl cursor-pointer"
+          >
+            <img
+              src={user.avatar}
+              alt={`${user.first_name} ${user.last_name}`}
+              className="w-24 h-24 rounded-full object-cover mb-4 shadow-md"
+            />
+            <div className="text-center">
+              <p className="text-lg font-semibold text-gray-900">
+                {user.first_name} {user.last_name}
+              </p>
+              <p className="text-sm text-gray-600">{user.email}</p>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <div className="col-span-full flex justify-center">
+          <p className="text-lg font-semibold text-red-600 bg-red-50 border border-red-300 rounded-lg p-4 shadow-md max-w-md">
+            User not found
+          </p>
+        </div>
+      )}
     </div>
   );
 };
